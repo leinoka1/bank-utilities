f452640188d1f2806c611812e537379ee41dd250
leinoka1 <kari.v.leino@gmail.com>
2017-09-19 10:14:59 +0300

    Added bank-utilities.csproj test program

---
+++ bank-utilities/bank-validator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ekoodi.Utilities.Test;
+namespace bank_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestUtility.PrintGreetings();
+            Console.ReadLine();
+        }
+    }
+}
